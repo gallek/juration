4fba34f90d65014f84a4c5968228eedd489b21af
King Elisha <ek.chibueze@gmail.com>
2022-01-18 15:11:23 +0100

    moved formats

--- languages/fr-FR.js
+++ languages/fr-FR.js
@@ -14,55 +14,55 @@
     juration.UNITS.weeks.patterns = ['semaine', 'sem', 'se'];
     juration.UNITS.months.patterns = ['mois'];
     juration.UNITS.years.patterns = ['année', 'ann', 'an'];
-  }
 
-  juration.UNITS.seconds.formats = {
-    chrono: '',
-    micro: 's',
-    short: 'sec',
-    long: 'seconde',
-    plural: 'secondes',
-  };
-  juration.UNITS.minutes.formats = {
-    chrono: ':',
-    micro: 'm',
-    short: 'min',
-    long: 'minute',
-    plural: 'minutes',
-  };
-  juration.UNITS.hours.formats = {
-    chrono: ':',
-    micro: 'h',
-    short: 'hre',
-    long: 'heure',
-    plural: 'heures',
-  };
-  juration.UNITS.days.formats = {
-    chrono: ':',
-    micro: 'j',
-    short: 'j',
-    long: 'jour',
-    plural: 'jours',
-  };
-  juration.UNITS.weeks.formats = {
-    chrono: ':',
-    micro: 'se',
-    short: 'sem',
-    long: 'semaine',
-    plural: 'semaines',
-  };
-  juration.UNITS.months.formats = {
-    chrono: ':',
-    micro: 'mois',
-    short: 'mois',
-    long: 'mois',
-    plural: 'mois',
-  };
-  juration.UNITS.years.formats = {
-    chrono: ':',
-    micro: 'an',
-    short: 'ann',
-    long: 'année',
-    plural: 'années',
-  };
+    juration.UNITS.seconds.formats = {
+      chrono: '',
+      micro: 's',
+      short: 'sec',
+      long: 'seconde',
+      plural: 'secondes',
+    };
+    juration.UNITS.minutes.formats = {
+      chrono: ':',
+      micro: 'm',
+      short: 'min',
+      long: 'minute',
+      plural: 'minutes',
+    };
+    juration.UNITS.hours.formats = {
+      chrono: ':',
+      micro: 'h',
+      short: 'hre',
+      long: 'heure',
+      plural: 'heures',
+    };
+    juration.UNITS.days.formats = {
+      chrono: ':',
+      micro: 'j',
+      short: 'j',
+      long: 'jour',
+      plural: 'jours',
+    };
+    juration.UNITS.weeks.formats = {
+      chrono: ':',
+      micro: 'se',
+      short: 'sem',
+      long: 'semaine',
+      plural: 'semaines',
+    };
+    juration.UNITS.months.formats = {
+      chrono: ':',
+      micro: 'mois',
+      short: 'mois',
+      long: 'mois',
+      plural: 'mois',
+    };
+    juration.UNITS.years.formats = {
+      chrono: ':',
+      micro: 'an',
+      short: 'ann',
+      long: 'année',
+      plural: 'années',
+    };
+  }
 })();
