dfb4677358dfcd891dfb54071d0171a481675e9f
King Elisha <ek.chibueze@gmail.com>
2022-01-21 22:36:00 +0100

    updated language selection logic

--- languages/de.js
+++ languages/de.js
@@ -1,68 +1,63 @@
-(function(){
-  var juration;
-  if (typeof require !== 'undefined')
-    juration = require('juration');
-  else
-    juration = window.juration;
-  
-  if (juration && juration.UNITS){
-    juration.UNITS.seconds.patterns = ['Sekunde', 'Sekunden', 'sek', 's'];
-    juration.UNITS.minutes.patterns = ['Minute', 'Minuten', 'min', 'm(?!s)'];
-    juration.UNITS.hours.patterns = ['Stunde', 'Stunden', 'h'];
-    juration.UNITS.days.patterns = ['Tag', 'Tage', 'T', 't'];
-    juration.UNITS.weeks.patterns = ['Woche', 'Wochen', 'w'];
-    juration.UNITS.months.patterns = ['Monat', 'Monate', 'M', 'Mo', 'Mnt'];
-    juration.UNITS.years.patterns = ['Jahr', 'Jahre', 'J', 'Jr'];
-    
-    juration.UNITS.seconds.formats = {
-      'chrono': '',
-      'micro':  's',
-      'short':  'sek',
-      'long':   'Sekunde',
-      'plural': 'Sekunden'
+const setGermanUnits = UNITS => {
+  if (UNITS) {
+    UNITS.seconds.patterns = ['Sekunde', 'Sekunden', 'sek', 's'];
+    UNITS.minutes.patterns = ['Minute', 'Minuten', 'min', 'm(?!s)'];
+    UNITS.hours.patterns = ['Stunde', 'Stunden', 'h'];
+    UNITS.days.patterns = ['Tag', 'Tage', 'T', 't'];
+    UNITS.weeks.patterns = ['Woche', 'Wochen', 'w'];
+    UNITS.months.patterns = ['Monat', 'Monate', 'M', 'Mo', 'Mnt'];
+    UNITS.years.patterns = ['Jahr', 'Jahre', 'J', 'Jr'];
+
+    UNITS.seconds.formats = {
+      chrono: '',
+      micro: 's',
+      short: 'sek',
+      long: 'Sekunde',
+      plural: 'Sekunden',
     };
-    juration.UNITS.minutes.formats = {
-      'chrono': ':',
-      'micro':  'm',
-      'short':  'min',
-      'long':   'Minute',
-      'plural': 'Minuten'
+    UNITS.minutes.formats = {
+      chrono: ':',
+      micro: 'm',
+      short: 'min',
+      long: 'Minute',
+      plural: 'Minuten',
     };
-    juration.UNITS.hours.formats = {
-      'chrono': ':',
-      'micro':  'h',
-      'short':  'h',
-      'long':   'Stunde',
-      'plural': 'Stunden'
+    UNITS.hours.formats = {
+      chrono: ':',
+      micro: 'h',
+      short: 'h',
+      long: 'Stunde',
+      plural: 'Stunden',
     };
-    juration.UNITS.days.formats = {
-      'chrono': ':',
-      'micro':  'T',
-      'short':  'Tag',
-      'long':   'Tag',
-      'plural': 'Tage'
+    UNITS.days.formats = {
+      chrono: ':',
+      micro: 'T',
+      short: 'Tag',
+      long: 'Tag',
+      plural: 'Tage',
     };
-    juration.UNITS.weeks.formats = {
-      'chrono': ':',
-      'micro':  'W',
-      'short':  'W',
-      'long':   'Woche',
-      'plural': 'Wochen'
+    UNITS.weeks.formats = {
+      chrono: ':',
+      micro: 'W',
+      short: 'W',
+      long: 'Woche',
+      plural: 'Wochen',
     };
-    juration.UNITS.months.formats = {
-      'chrono': ':',
-      'micro':  'M',
-      'short':  'M',
-      'long':   'Monat',
-      'plural': 'Monate'
+    UNITS.months.formats = {
+      chrono: ':',
+      micro: 'M',
+      short: 'M',
+      long: 'Monat',
+      plural: 'Monate',
     };
-    juration.UNITS.years.formats = {
-      'chrono': ':',
-      'micro':  'J',
-      'short':  'J',
-      'long':   'Jahr',
-      'plural': 'Jahre'
+    UNITS.years.formats = {
+      chrono: ':',
+      micro: 'J',
+      short: 'J',
+      long: 'Jahr',
+      plural: 'Jahre',
     };
   }
-})();
+};
 
+module.exports = setGermanUnits;
