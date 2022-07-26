b352204a8dc6d1e23955e978990355f9b3b97f8e
gallek <gallek@oregonstate.edu>
2022-07-26 09:46:43 -0700

    Testing out Japanese fix

--- languages/ja-JP.js
+++ languages/ja-JP.js
@@ -13,42 +13,49 @@ const setJapaneseUnits = UNITS => {
       micro: 's',
       short: '秒',
       long: '秒',
+      plural: '秒',
     };
     UNITS.minutes.formats = {
       chrono: ':',
       micro: 'm',
       short: '分',
       long: '分',
+      plural: '分',
     };
     UNITS.hours.formats = {
       chrono: ':',
       micro: 'h',
       short: '時間',
       long: '時間',
+      plural: '時間',
     };
     UNITS.days.formats = {
       chrono: ':',
       micro: '日',
       short: '日',
       long: '日',
+      plural: '日',
     };
     UNITS.weeks.formats = {
       chrono: ':',
       micro: '週',
       short: '週間',
       long: '週間',
+      plural: '週間',
     };
     UNITS.months.formats = {
       chrono: ':',
       micro: 'か月',
       short: 'か月',
       long: 'か月',
+      plural: 'か月',
     };
     UNITS.years.formats = {
       chrono: ':',
       micro: '年',
       short: '年',
       long: '年',
+      plural: '年',
     };
   }
 };
